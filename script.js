f10bf283491f9c55d807867731430cc4ef7a29db
DanielFarrokhahmadi <dgeek8213@gmail.com>
2022-08-08 16:24:24 +0430

    Fixed issues caused by reseting sketchbook

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ function drawGrid(x = 16, y = 16) {
     let grid = document.createElement('div');
     grid.style.display = 'flex';
     grid.style.flexDirection = 'column';
-    grid.style.border = 'solid';
+    grid.style.border = 'solid #f9faf8';
     grid.style.flexBasis = '80%';
-    grid.style.width = '80%';
+    grid.style.width = '60%';
+    grid.style.backgroundColor = '#f9faf8';
     grid.className = 'grid';
     container.appendChild(grid);
 
@@ -29,28 +30,34 @@ function drawGrid(x = 16, y = 16) {
     }
 }
 
-drawGrid()
+function sketch() {
+    let color = document.querySelector('.colorpicker');
+    const cells = Array.from(document.querySelectorAll('.cell'));
+    cells.forEach(cell => cell.addEventListener('mouseover', function handleMouseover() {
+    cell.style.backgroundColor = `${color.value}`;
+    }));
+}
 
-let color = document.querySelector('.colorpicker');
-const cells = Array.from(document.querySelectorAll('.cell'));
-cells.forEach(cell => cell.addEventListener('mouseover', function handleMouseover() {
- cell.style.backgroundColor = `${color.value}`;
-}));
+drawGrid()
+sketch()
 
 
 function clearSketchbook() {
+    const cells = Array.from(document.querySelectorAll('.cell'));
     cells.forEach(cell => cell.style.backgroundColor = 'white');
 }
 
 function resetSketchbook() {
     let grid = document.querySelector('.grid');
     container.removeChild(grid);
-    let input = prompt("enter the number of squares per side for the new grid (format = i x j)");
+    let input = prompt("enter the number of squares per side for the new grid (format = i x j) (the borders might not show up for grids with more than 2,500 squares)");
     const inputArray = input.split(" ");
     let x = inputArray[0];
     let y = inputArray[2];
+    
     if (0 < x < 100  && 0 < y < 100) {
         drawGrid(x, y)
+        sketch()
     }
 
     else {
@@ -59,6 +66,7 @@ function resetSketchbook() {
 }
 
 function changeBorders() {
+    const cells = Array.from(document.querySelectorAll('.cell'));
     let borders_btn = document.querySelector('.borders_btn');
     if (borders_btn.textContent == 'show borders') {
         borders_btn.textContent = 'hide borders'
@@ -71,8 +79,6 @@ function changeBorders() {
         cells.forEach(cell => cell.classList.remove('bordersShown'));
     }
 }
-
-
 
 let clr_btn = document.querySelector('.clr_btn');
 let rst_btn = document.querySelector('.rst_btn');
